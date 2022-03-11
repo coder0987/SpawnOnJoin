24e103d93e7412d52d65696a3cca6370172e70d2
Robotics <robotics@umeprep.org>
2022-03-11 16:14:44 -0600

    Initial commit

---
+++ src/main/java/SpawnOnJoin.java
@@ -0,0 +1,28 @@
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class SpawnOnJoin extends JavaPlugin implements Listener {
+
+    //The onEnable method runs as the plugin boots up and when the server reloads
+    @Override
+    public void onEnable() {
+        // Enable our class to check for new players using onPlayerJoin()
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    // This method checks for incoming players and sends them to spawn
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        final Player player = event.getPlayer();
+        player.teleport(getServer().getWorlds().get(0).getSpawnLocation());
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
